3c06f700c4d09a17665621ac23877ad72b9ec87e
linson0116 <linson0116@163.com>
2017-07-11 23:23:27 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import com.example.demo.utils.ActivitiUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by linson on 2017/7/11.
+ */
+@RestController
+public class MyController {
+    @RequestMapping("deploy")
+    public void deploy() {
+        System.out.println("deploy");
+        String resName = "helloworld";//
+        String depName = "hello";
+        ActivitiUtils activitiUtils = new ActivitiUtils();
+        activitiUtils.deploymentProcessDefinition(depName,resName);
+    }
+    @RequestMapping("find")
+    public void find() {
+        System.out.println("find");
+
+        ActivitiUtils activitiUtils = new ActivitiUtils();
+        activitiUtils.findProcessDefinition();
+    }
+}
